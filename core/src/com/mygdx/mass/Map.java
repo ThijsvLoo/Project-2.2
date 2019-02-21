c8d9f2b2bf1689add2e203f6c90a5cf626184957
roeldkeum <r.hensels@student.maastrichtuniversity.nl>
2019-02-21 14:20:58 +0100

    blyat3

--- core/src/com/mygdx/mass/Map.java
+++ core/src/com/mygdx/mass/Map.java
@@ -2,8 +2,8 @@ package com.mygdx.mass;
 
 public class Map {
 
-    public static final double WIDTH = 200;
-    public static final double HEIGHT = 200;
+    private static final double WIDTH = 200;
+    private static final double HEIGHT = 200;
 
     public static double getWIDTH() {
         return WIDTH;
