52a957a85e2de17a49cf10184c9b1fae4068ab08
roeldkeum <r.hensels@student.maastrichtuniversity.nl>
2019-02-21 15:00:49 +0100

    blyat5

--- core/src/com/mygdx/mass/Entity/SentryTower.java
+++ core/src/com/mygdx/mass/Entity/SentryTower.java
@@ -1,4 +1,6 @@
 package com.mygdx.mass.Entity;
 
 public class SentryTower extends Entity {
+    // they are visible from 18m away
+    // spotting if they're manned is normal range
 }
--- core/src/com/mygdx/mass/Entity/TargetArea.java
+++ core/src/com/mygdx/mass/Entity/TargetArea.java
@@ -1,4 +1,7 @@
 package com.mygdx.mass.Entity;
 
 public class TargetArea extends Entity {
+    // unknown to guards
+    // if intruder stands in this area for 3 seconds they win
+    // if intruder visits this area twice with more than 3 seconds apart, they also win
 }
