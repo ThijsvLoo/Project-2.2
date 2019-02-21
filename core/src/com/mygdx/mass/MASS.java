e13dc87d845f72dc2331b4e6dbc4f1e3c8d72be1
Hong <Honghai.Ngo@hotmail.com>
2019-02-21 13:20:32 +0100

    Added MapBuilderScreen

--- core/src/com/mygdx/mass/MASS.java
+++ core/src/com/mygdx/mass/MASS.java
@@ -6,29 +6,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.mass.Screens.MapBuilderScreen;
 
 public class MASS extends Game {
-	SpriteBatch batch;
-	Texture img;
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new MapBuilderScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/mygdx/mass/Screens/MapBuilderScreen.java
@@ -0,0 +1,58 @@
+package com.mygdx.mass.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.mass.MASS;
+
+public class MapBuilderScreen implements Screen {
+
+    private MASS mass;
+    Texture texture;
+
+    public MapBuilderScreen(MASS mass) {
+        this.mass = mass;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        mass.batch.begin();
+        mass.batch.draw(texture,0,0);
+        mass.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
